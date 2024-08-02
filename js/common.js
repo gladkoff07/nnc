c82e36f9a46faa6dfd0b0c6bd4186ebbe12b9911
gladkoff07 <t-orange@yandex.ru>
2024-08-02 11:21:38 +0300

    add menu catalog

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector('.js-submenu').addEventListener('click', function (e) {
+    e.preventDefault();
+    this.closest('.menu__item').querySelector('.menu__sub').classList.toggle('menu__sub--active');
+  });
+  document.querySelector('.close-sub-menu').addEventListener('click', function () {
+    this.closest('.menu__sub').classList.remove('menu__sub--active');
+  });
+});
